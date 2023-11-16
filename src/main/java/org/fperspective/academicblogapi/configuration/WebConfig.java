98d936ef9551d350ac77ebb5d7d176f0159c1a6e
WaveInf <ngnphucan@gmail.com>
2023-11-16 17:58:38 +0700

    fix cors

--- src/main/java/org/fperspective/academicblogapi/configuration/WebConfig.java
+++ src/main/java/org/fperspective/academicblogapi/configuration/WebConfig.java
@@ -15,9 +15,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        String test = frontendUrl + "/login";
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl, test) // your reactjs URL
+                .allowedOrigins("*") // your reactjs URL
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "Authorization") // Adjust headers you need to allow
